278a1cee075b50480f4361916c5245f5cdf631ce
dekanbro <dekanbrown@odyssy.io>
2022-06-01 10:15:51 -0600

    update deploy addresses

--- src/util/contractAddresses.ts
+++ src/util/contractAddresses.ts
@@ -3,34 +3,63 @@ interface GnosisContracts {
   gnosisSingleton: string
   gnosisMultisend: string
   gnosisFallback: string
+  gnosisSafeProxyFactory: string
+  gnosisModuleProxyFactory: string
 }
+
 
 export const rinkeby: GnosisContracts = {
  gnosisSingleton:  '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
  gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
- gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4'
+ gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+ gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+ gnosisModuleProxyFactory: '0x00000000062c52e29e8029dc2413172f6d619d85',
 }
 
 export const kovan: GnosisContracts = {
  gnosisSingleton:  '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
  gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
- gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4'
+ gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+ gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+ gnosisModuleProxyFactory: '0x00000000062c52e29e8029dc2413172f6d619d85',
 }
 
 export const mainnet: GnosisContracts = {
  gnosisSingleton:  '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
  gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
- gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4'
+ gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+ gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+ gnosisModuleProxyFactory: '0x00000000062c52e29e8029dc2413172f6d619d85',
 }
+
+export const arbitrum: GnosisContracts = {
+  gnosisSingleton:  '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
+  gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
+  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+  gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+  gnosisModuleProxyFactory: '0x00000000062c52e29e8029dc2413172f6d619d85',
+ }
+
+ export const optimism: GnosisContracts = {
+  gnosisSingleton:  '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552',
+  gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
+  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+  gnosisSafeProxyFactory: '0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC',
+  gnosisModuleProxyFactory: '',
+ }
 
 export const xdai: GnosisContracts = {
   gnosisSingleton: '0x3E5c63644E683549055b9Be8653de26E0B4CD36E', // gnosis l2
   gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
-  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4'
+  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+  gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+  gnosisModuleProxyFactory: '',
 }
 
 export const polygon: GnosisContracts = {
   gnosisSingleton: '0x3E5c63644E683549055b9Be8653de26E0B4CD36E', // gnosis l2
   gnosisMultisend: '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761',
-  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4'
+  gnosisFallback: '0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4',
+  gnosisSafeProxyFactory: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+  gnosisModuleProxyFactory: '0x00000000062c52e29e8029dc2413172f6d619d85',
 }
