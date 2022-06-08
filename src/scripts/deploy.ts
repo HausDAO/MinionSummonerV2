d58d038ff6081cf153288a5385d416c11b1fa3f7
dekanbro <dekanbrown@odyssy.io>
2022-06-08 08:49:58 -0600

    update deploy script

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -1,62 +1,34 @@
-import { ethers } from "hardhat";
-import { SafeMinion } from "../types/SafeMinion";
-import { SafeMinionSummoner } from "../types/SafeMinionSummoner";
-import {
-  kovan,
-  mainnet,
-  polygon,
-  rinkeby,
-  xdai,
-  goerli,
-  arbitrum
-  kovanOptimism, 
-  optimism,
-} from "../util/contractAddresses";
-
+import { ethers } from 'hardhat'
+import { SafeMinion } from '../types/SafeMinion'
+import { SafeMinionSummoner } from '../types/SafeMinionSummoner'
+import { kovan, mainnet, polygon, rinkeby, xdai } from '../util/contractAddresses'
 
 async function main() {
-  const accounts = await ethers.getSigners();
+  const accounts = await ethers.getSigners()
 
   console.log(
-    "Accounts:",
+    'Accounts:',
     accounts.map((a) => a.address)
-  );
-
+  )
+  
   // const contractAddresses = polygon
   // const contractAddresses = mainnet
-  const contractAddresses = optimism
-  // const contractAddresses = arbitrum
   // const contractAddresses = mainnet
-  // const contractAddresses = optimism
-  // const contractAddresses = xdai;
-  // const contractAddresses = rinkeby;
-  // const contractAddresses = goerli;
-  // const contractAddresses = kovan;
-
-  const SafeMinion = await ethers.getContractFactory("SafeMinion");
-  const SafeMinionSummoner = await ethers.getContractFactory(
-    "SafeMinionSummoner"
-  );
-
+  const contractAddresses = xdai
+  
+  const SafeMinion = await ethers.getContractFactory('SafeMinion')
+  const SafeMinionSummoner = await ethers.getContractFactory('SafeMinionSummoner')
   // const ConditionalMinionTemplate = await ethers.getContractFactory('ConditionalMinion')
   // const ConditionalMinionSummoner = await ethers.getContractFactory('ConditionalMinionFactory')
   // const ERC1271MinionTemplate = await ethers.getContractFactory('ERC1271Minion')
   // const ERC1271MinionSummoner = await ethers.getContractFactory('ERC1271MinionFactory')
-  console.log("ready for deploy");
+  console.log('ready for deploy')
 
-  // setting gas limits on arbitrum will not work
-  // const safeMinionTemplate = (await SafeMinion.deploy()) as SafeMinion;
-  // console.log({ safeMinionTemplate });
-  // await safeMinionTemplate.deployTransaction.wait();
-  console.log("safe deployed");
-  const safeMinionSummoner = (await SafeMinionSummoner.deploy(
-    "0x51498dddd2a8cdec82932e08a37ebaf346c38efd",
-    contractAddresses.gnosisSingleton,
-    contractAddresses.gnosisFallback,
-    contractAddresses.gnosisMultisend,
-    contractAddresses.gnosisSafeProxyFactory,
-    contractAddresses.gnosisModuleProxyFactory
-  )) as SafeMinionSummoner;
+  const safeMinionTemplate = (await SafeMinion.deploy({gasLimit: 3000000, gasPrice: 80000000000})) as SafeMinion
+  console.log({safeMinionTemplate})
+  await safeMinionTemplate.deployTransaction.wait()
+  console.log('safe deployed')
+  const safeMinionSummoner = (await SafeMinionSummoner.deploy(safeMinionTemplate.address, contractAddresses.gnosisSingleton, contractAddresses.gnosisFallback, contractAddresses.gnosisMultisend, {gasPrice: 80000000000})) as SafeMinionSummoner
 
   // const conditionalMinionTemplate = (await ConditionalMinionTemplate.deploy()) as ConditionalMinion
   // const conditionalMinionFactory = await ConditionalMinionSummoner.deploy(conditionalMinionTemplate.address)
@@ -64,27 +36,19 @@ async function main() {
   // const erc1271MinionTemplate = (await ERC1271MinionTemplate.deploy()) as Erc1271Minion
   // const erc1271MinionFactory = await ERC1271MinionSummoner.deploy(erc1271MinionTemplate.address)
 
-  console.log("waiting for deployment");
+  console.log('waiting for deployment')
 
   console.log({
     safeMinionSummoner: safeMinionSummoner.address,
-    safeMinionTemplate: "0x51498dddd2a8cdec82932e08a37ebaf346c38efd",
+    safeMinionTemplate: safeMinionTemplate.address,
     // conditionalMinionFactory: conditionalMinionFactory.address,
     // erc1271MinionFactory: erc1271MinionFactory.address,
-  });
-
-  console.log('deploy params', "0x51498dddd2a8cdec82932e08a37ebaf346c38efd",
-  contractAddresses.gnosisSingleton,
-  contractAddresses.gnosisFallback,
-  contractAddresses.gnosisMultisend,
-  contractAddresses.gnosisSafeProxyFactory,
-  contractAddresses.gnosisModuleProxyFactory);
-  
+  })
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
